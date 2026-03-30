25f429ca06f9b8b379e6de6fba281679889c81ec
Satya <satyaranjannayak799@gmail.com>
2026-03-30 15:10:58 +0530

    chore: initial spring boot project setup

---
+++ src/main/java/com/satya/network_anomaly_detection/NetworkAnomalyDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.satya.network_anomaly_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAnomalyDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkAnomalyDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satya/network_anomaly_detection/NetworkAnomalyDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya.network_anomaly_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkAnomalyDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
