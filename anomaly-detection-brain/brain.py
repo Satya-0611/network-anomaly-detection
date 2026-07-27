bace708806aad823e26d5f98c1d945b10ab0e722
Satyaranjan Nayak <satyaranjannayak799@gmail.com>
2026-07-27 09:00:04 +0530

    Dockerized java and python server

--- anomaly-detection-brain/brain.py
+++ anomaly-detection-brain/brain.py
@@ -26,7 +26,7 @@ is_trained = False
 
 consumer = KafkaConsumer(
     'raw-network-logs',
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['kafka:29092'],
     auto_offset_reset='latest',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
     consumer_timeout_ms=1000
